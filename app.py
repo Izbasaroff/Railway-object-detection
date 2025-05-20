ba6ef43b17d6a24e0648f45c0d51e274c6d486e4
Aidos Izbassarov <izbasar.aidos@gmail.com>
2025-05-21 01:39:15 +0500

    Добавил app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 import streamlit as st
 from ultralytics import YOLO
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 import numpy as np
-import yaml
-
-# Загружаем кастомные имена классов
-with open("data.yaml") as f:
-    data = yaml.safe_load(f)
-classes = data['names']  # используем это!
 
 st.title("🚆 Railway Object Detection")
 
@@ -20,25 +14,14 @@ model = load_model()
 uploaded_file = st.file_uploader("Загрузите изображение (JPG, PNG)", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
+
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Оригинальное изображение", use_container_width=True)
 
     img_array = np.array(image)
+
     results = model(img_array)
-    result = results[0]
 
-    # Копируем изображение для кастомной отрисовки
-    annotated_image = image.copy()
-    draw = ImageDraw.Draw(annotated_image)
-    font = ImageFont.load_default()
+    annotated_img = results[0].plot()
 
-    for box in result.boxes:
-        cls_id = int(box.cls[0].item())
-        conf = box.conf[0].item()
-        label = f"{classes[cls_id]} {conf:.2f}"
-
-        x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
-        draw.rectangle([x1, y1, x2, y2], outline="red", width=2)
-        draw.text((x1, y1 - 10), label, fill="red", font=font)
-
-    st.image(annotated_image, caption="Распознанные объекты", use_container_width=True)
+    st.image(annotated_img, caption="Распознанные объекты", use_container_width=True)
