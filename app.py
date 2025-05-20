d3852abaf31a30386560ac56b4276f9269d6b0f9
Aidos Izbassarov <izbasar.aidos@gmail.com>
2025-05-21 01:28:29 +0500

    Добавил app.py

--- app.py
+++ app.py
@@ -28,6 +28,16 @@ if uploaded_file is not None:
     results = model(img_array)
 
     annotated_img = results[0].plot()
+    
+    for box in results.boxes:
+        cls_id = int(box.cls[0].item())
+        conf = box.conf[0].item()
+        label = f"{custom_names[cls_id]} {conf:.2f}"
+
+        x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+        draw.rectangle([x1, y1, x2, y2], outline="red", width=2)
+        draw.text((x1, y1 - 10), label, fill="red", font=font)
+
 
 
     st.image(annotated_img, caption="Распознанные объекты", use_container_width=True)
