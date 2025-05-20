ce571d6032ba2a36486968004c0c460df5aa926a
Aidos Izbassarov <izbasar.aidos@gmail.com>
2025-05-21 01:21:20 +0500

    Добавил app.py

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from ultralytics import YOLO
 from PIL import Image
 import numpy as np
 import io
+import yaml
+
+with open("data.yaml") as f:
+    data = yaml.safe_load(f)
+classes = data['names']
 
 st.title("🚆 Railway Object Detection")
 
-# Загружаем модель один раз при старте
 @st.cache_resource
 def load_model():
     return YOLO("best.pt")
@@ -16,18 +20,14 @@ model = load_model()
 uploaded_file = st.file_uploader("Загрузите изображение (JPG, PNG)", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Открываем изображение
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Оригинальное изображение", use_container_width=True)
 
-    # Конвертируем в numpy-массив для модели
     img_array = np.array(image)
 
-    # Запускаем предсказание
     results = model(img_array)
 
-    # Получаем изображение с нанесёнными бокcами и метками
     annotated_img = results[0].plot()
 
-    # Показываем результат
+
     st.image(annotated_img, caption="Распознанные объекты", use_container_width=True)
