35129bd35a49cbb06b2c140b14ae1e43b1cf2deb
Aidos Izbassarov <izbasar.aidos@gmail.com>
2025-05-21 09:25:29 +0500

    Добавил app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ model = load_model()
 uploaded_file = st.file_uploader("Загрузите изображение (JPG, PNG)", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Оригинальное изображение", use_container_width=True)
 
@@ -22,6 +21,16 @@ if uploaded_file is not None:
 
     results = model(img_array)
 
-    annotated_img = results[0].plot()
+    # Проверка на наличие класса 0
+    detected_class_0 = False
+    for box in results[0].boxes:
+        cls_id = int(box.cls[0].item())
+        if cls_id == 0:
+            detected_class_0 = True
+            break
 
+    if detected_class_0:
+        st.error("🚨 Обнаружен объект класса 0!")
+
+    annotated_img = results[0].plot()
     st.image(annotated_img, caption="Распознанные объекты", use_container_width=True)
